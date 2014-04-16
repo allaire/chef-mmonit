dde0f80d1198c28a14de40330cc0bdb3bc2fc2cb
Mathieu Allaire <mathieuallaire@gmail.com>
2014-04-15 21:14:18 -0400

    Bump MMonit version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,8 @@
-default['mmonit']['ip'] = node['ipaddress']
+default['mmonit']['host'] = node['ipaddress']
 default['mmonit']['port'] = 8080
 default['mmonit']['dir'] = '/usr/local/mmonit'
-default['mmonit']['source']['version'] = '3.1.1'
+default['mmonit']['source']['version'] = '3.1.3'
 default['mmonit']['source']['filename'] = "mmonit-#{node['mmonit']['source']['version']}-linux-x64.tar.gz"
 default['mmonit']['source']['url'] = "http://mmonit.com/dist/#{node['mmonit']['source']['filename']}"
+default['mmonit']['license_owner'] = "Tildeslash Ltd. - Trial License"
+default['mmonit']['license'] = nil
