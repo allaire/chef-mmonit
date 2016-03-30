eb00adbf6bfe999ccf5520117dd699c1b078dc8d
Mathieu Allaire <mathieuallaire@gmail.com>
2016-03-30 08:52:56 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             "mmonit"
-maintainer       "Lodgem Inc."
-maintainer_email "dev@lodgem.com"
+maintainer       "Mathieu Allaire"
+maintainer_email "mathieuallaire@gmail.com"
 license          "New BSD license"
 description      "Installs/Configures M/Monit"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
